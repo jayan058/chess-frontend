20799f93def5b630c6d0158784b3d1d494ce2e88
jayan058 <jghimire.034@gmail.com>
2024-07-27 18:19:54 +0545

    started working on displaying players past game history

--- chess-frontend/src/main.ts
+++ chess-frontend/src/main.ts
@@ -1,49 +1,44 @@
 import { Router } from "./router";
-import socketInstance from './utils/socket'; // Import the SocketSingleton instance
+import socketInstance from "./utils/socket"; // Import the SocketSingleton instance
 
 document.addEventListener("DOMContentLoaded", () => {
-    Router.init();
-    let previousHash = window.location.hash;
+  Router.init();
+  let previousHash = window.location.hash;
 
-    window.addEventListener("hashchange", () => {
-        const oldHash = previousHash;
-        const newHash = window.location.hash;
+  window.addEventListener("hashchange", () => {
+    const oldHash = previousHash;
+    const newHash = window.location.hash;
 
-        // Define routes where socket should be disconnected
-        const disconnectRoutes = [
-            '#/welcome',
-            '#/login',
-            '#/home'
-        ];
+    // Define routes where socket should be disconnected
+    const disconnectRoutes = ["#/welcome", "#/login", "#/home"];
 
-        // Define routes where socket should be reconnected
-        const reconnectRoutes = [
-            '#/create-game',
-            '#/join-game'
-        ];
+    // Define routes where socket should be reconnected
+    const reconnectRoutes = ["#/create-game", "#/join-game"];
 
-        // Check if socket should be disconnected on new hash
-        if (disconnectRoutes.includes(newHash)) {
-            socketInstance.disconnect();
-        }
+    // Check if socket should be disconnected on new hash
+    if (disconnectRoutes.includes(newHash)) {
+      socketInstance.disconnect();
+    }
 
-        // Load the new content
-        Router.loadContent();
+    // Load the new content
+    Router.loadContent();
 
-        // Logic to disconnect and reconnect the socket based on old and new hash
-        const shouldReconnect = (oldHash === '#/online' || oldHash === '#/welcome') && reconnectRoutes.includes(newHash);
-        
-        if (shouldReconnect) {
-            socketInstance.disconnect();
-            setTimeout(() => {
-                socketInstance.reconnect();
-            }, 100); // Adjust the timeout duration as needed
-        } else if (reconnectRoutes.includes(newHash) && !shouldReconnect) {
-            setTimeout(() => {
-                socketInstance.reconnect();
-            }, 100); // Adjust the timeout duration as needed
-        }
+    // Logic to disconnect and reconnect the socket based on old and new hash
+    const shouldReconnect =
+      (oldHash === "#/online" || oldHash === "#/welcome") &&
+      reconnectRoutes.includes(newHash);
 
-        previousHash = newHash; // Update the previous hash
-    });
+    if (shouldReconnect) {
+      socketInstance.disconnect();
+      setTimeout(() => {
+        socketInstance.reconnect();
+      }, 100);
+    } else if (reconnectRoutes.includes(newHash) && !shouldReconnect) {
+      setTimeout(() => {
+        socketInstance.reconnect();
+      }, 100);
+    }
+
+    previousHash = newHash; // Update the previous hash
+  });
 });
